70f18411426e17671f3c6e891fe3296529e13cd8
sabbir1054 <mdsabbir1054@gmail.com>
2024-08-07 16:02:28 +0600

    register and login logout working

--- src/shared/NavigationBars/HeroNavigationBar/AvaterBtnMeny.jsx
+++ src/shared/NavigationBars/HeroNavigationBar/AvaterBtnMeny.jsx
@@ -10,7 +10,7 @@ import {
   Typography,
 } from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const pages = ["Products", "Pricing", "Blog"];

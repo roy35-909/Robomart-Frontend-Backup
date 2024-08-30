27cc5d3d27fafe26b3d1b11ca5adacfeb24987f2
sabbir1054 <mdsabbir1054@gmail.com>
2024-08-30 09:05:21 +0600

    update all single sub category section

--- src/app/(withCommonLayout)/products/scategories/[subCategoryId]/[subCategoryName]/_SingleSubCategoryProduct/SubCategoryProducts.jsx
+++ src/app/(withCommonLayout)/products/scategories/[subCategoryId]/[subCategoryName]/_SingleSubCategoryProduct/SubCategoryProducts.jsx
@@ -1,53 +1,64 @@
 "use client";
 
-import { Box, Divider, FormControl, Grid, InputLabel, MenuItem, Pagination, Select, Typography } from "@mui/material";
-import { lazy, Suspense, useState } from "react";
+import AllCategorySideMenu from "@/Shared/AllCategoryListSideMenu/AllCategorySideMenu";
+import CategoryWiseProductLoading from "@/components/Skeletons/Home/CategoryWiseProductLoading";
+import { useGetCategoryListProductsQuery } from "@/redux/api/api";
+import {
+  Box,
+  Divider,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Pagination,
+  Select,
+  Typography,
+} from "@mui/material";
+import { lazy, Suspense, useEffect, useState } from "react";
 const SingleProductCard = lazy(() =>
   import("@/Shared/SingleProductCard/SingleProductCard")
 );
 
-import AllCategorySideMenu from "@/Shared/AllCategoryListSideMenu/AllCategorySideMenu";
-import CategoryWiseProductLoading from "@/components/Skeletons/Home/CategoryWiseProductLoading";
-import { useGetCategoryListProductsQuery } from "@/redux/api/api";
-
-const SubCategoryProducts = ({data, params }) => {
+const SubCategoryProducts = ({ data, params }) => {
   const {
     data: categoryList,
     isLoading: catLoading,
     isError,
   } = useGetCategoryListProductsQuery();
-  const [isLoading, setIsLoading] = useState(false);
   const [categoryProducts, setCategoryProducts] = useState(
     data?.length > 0 ? data : []
   );
   const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemPerPage] = useState(50); // Set the number of items per page
-
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   fetch(`${backendUrl}/api/catagory/${params.subCategoryId}/subcategory`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setCategoryProducts(data);
-  //       setIsLoading(false);
-  //     });
-  // }, [params]);
+  const [itemsPerPage, setItemsPerPage] = useState(50); // Set the number of items per page
+  const [loading, setLoading] = useState(false);
 
   // Pagination logic
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = categoryProducts.slice(
-    indexOfFirstItem,
-    indexOfLastItem
-  );
+  const currentItems =
+    categoryProducts?.length > 0 &&
+    categoryProducts.slice(indexOfFirstItem, indexOfLastItem);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
- const handleChange = (e) => {
-   setItemPerPage(e.target.value);
- };
+
+  const handleChange = (e) => {
+    setLoading(true);
+    setItemsPerPage(e.target.value);
+    setCurrentPage(1); // Reset to the first page after changing items per page
+
+    // Simulate loading time to show skeleton
+    setTimeout(() => {
+      setLoading(false);
+    }, 500); // Adjust the timeout as needed
+  };
+
+  useEffect(() => {
+    setCategoryProducts(data);
+    setLoading(catLoading);
+  }, [data, catLoading]);
+
   return (
     <div>
-      {" "}
       <Grid
         container
         sx={{ backgroundColor: "#f2f2f2", minHeight: "80vh" }}
@@ -57,15 +68,15 @@ const SubCategoryProducts = ({data, params }) => {
         <Grid item xs={2} padding={2} hidden={{ xs: true }}>
           {categoryList && <AllCategorySideMenu category={categoryList} />}
         </Grid>
-        <Grid item xs={10}>
-          {isLoading ? (
+        <Grid item xs={12} sm={12}>
+          {loading ? (
             <CategoryWiseProductLoading />
           ) : (
             <>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <div style={{ marginX: "5px" }}>
                   <Typography marginTop={3} variant="h6" fontFamily={"Poppins"}>
-                    {decodeURIComponent(params?.subCategoryName)} :
+                    {decodeURIComponent(params?.subCategoryNamegit )} :
                   </Typography>
                 </div>
                 <FormControl
@@ -104,13 +115,19 @@ const SubCategoryProducts = ({data, params }) => {
                   </Select>
                 </FormControl>
               </div>
+
               <Divider />
-              <Box
-                paddingY={1}
-                marginY={1}
-                display={"flex"}
-                justifyContent={"center"}
-              >
+              <Box paddingY={1} marginY={1}>
+                {categoryProducts?.length == 0 && !loading && (
+                  <Typography
+                    marginTop={3}
+                    variant="h6"
+                    textAlign={"center"}
+                    fontFamily={"Poppins"}
+                  >
+                    No products
+                  </Typography>
+                )}
                 <Box
                   paddingY={2}
                   display={"flex"}
@@ -123,7 +140,7 @@ const SubCategoryProducts = ({data, params }) => {
                       }
                     >
                       {currentItems?.length > 0 &&
-                        !isLoading &&
+                        !loading &&
                         currentItems?.map((product) => (
                           <Grid
                             item
@@ -132,7 +149,8 @@ const SubCategoryProducts = ({data, params }) => {
                             md={3}
                             lg={3}
                             xl={2}
-                           
+                            display={"flex"}
+                            justifyContent={"center"}
                             key={product.id}
                           >
                             <SingleProductCard product={product} />
@@ -145,7 +163,13 @@ const SubCategoryProducts = ({data, params }) => {
             </>
           )}
 
-          <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "5vh 0",
+            }}
+          >
             <Pagination
               color="success"
               count={Math.ceil(categoryProducts.length / itemsPerPage)}
